4b43b84172d8c5603cc1ebcf8cfb330b9d56c4ef
SUKHNAM GILL <163373978+sukhnamgill@users.noreply.github.com>
2024-10-04 07:06:07 +0530

    new files 10-4-24
    few more

---
+++ c programs/arithmetic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    printf("hello world\n");
+    int a= 1*2/1+5*3;
+    // so flow od associative is left to right
+    // first priority to */%
+    // second to +-
+    // third to =
+    //  now we solving this 
+    /*||| 1*2/1+5*3
+            2/1+5*3
+            2+5*3
+            2+15
+            17 answer 
+    
+    
+    */
+//    state and prove
+printf("the answer is %d \n",a);
+   
+   
+
+}
---
+++ c programs/chdtyp.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("hello world sukhnam.com\n");
+    int a=555;
+    float b=32.9;
+    a= (int) b;
+    printf("%d \n",a);
+
+}
